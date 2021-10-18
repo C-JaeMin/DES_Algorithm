ea31a89e4cd665870ac7ec52b4307c9716f7a137
C-JaeMin <77180020+C-JaeMin@users.noreply.github.com>
2021-10-19 02:02:18 +0900

    Add files via upload

---
+++ test.php
@@ -0,0 +1,43 @@
+<?php
+$conn = mysqli_connect("localhost", "jaem", "alsk!!0421", "jaem");
+ 
+$query = "select * from testTable";
+ 
+ 
+if($result = mysqli_query($conn, $query)){
+    $row_num = mysqli_num_rows($result);
+    
+    echo "{";
+    
+        echo "\"status\":\"OK\",";
+        
+        echo "\"rownum\":\"$row_num\",";
+    
+        echo "\"result\":";
+        
+            echo "[";
+            
+                for($i = 0; $i < $row_num; $i++){
+                    $row = mysqli_fetch_array($result);
+                    echo "{";
+                    
+                    echo "\"image\":\"$row[image]\", \"text\":\"$row[text]\"";
+                    
+                    echo "}";
+                    if($i<$row_num-1){
+                        echo ",";
+                    }
+                }
+ 
+                        
+                
+            echo "]";
+            
+    echo "}";
+}
+ 
+else{
+    echo "failed to get data from database.";
+}
+ 
+?>
